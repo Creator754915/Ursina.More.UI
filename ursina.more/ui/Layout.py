8a4bc2b0a6be7eed9073bc0a935714659e67846c
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2023-08-31 22:04:16 +0200

    Update Layout.py

--- ursina.more/ui/Layout.py
+++ ursina.more/ui/Layout.py
@@ -3,27 +3,43 @@ from ursina import *
 
 class Layout(Entity):
     def __init__(self, **kwargs):
-        super().__init__(model=Quad(radius=.025), color=color.gray, **kwargs)
-        self.color = self.color.tint(.2)
-        self.scale = (8, 8)
+        super().__init__(model=Quad(radius=.0), color=color.gray, **kwargs)
+        self.color = self.color.tint(-.05)
+        self.postion = (0, 5)
+        self.scale = (10, 5)
 
         if "side" in kwargs:
             print(kwargs['side'])
             if kwargs["side"] == "TOP":
                 self.position = (0, 5)
+                self.scale = (15, 5)
             elif kwargs["side"] == "LEFT":
                 self.position = (-8, 0)
+                self.scale = (5, 10)
             elif kwargs["side"] == "BOTTOM":
                 self.position = (0, -5)
+                self.scale = (15, 5)
             elif kwargs["side"] == "RIGHT":
                 self.position = (8, 0)
+                self.scale = (5, 10)
             else:
-                self.position = (0, 0)
+                self.position = (0, 5)
+                self.scale = (10, 5)
 
 
 if __name__ == "__main__":
     app = Ursina()
+    window.fullscreen = False
+    window.borderless = False
+    window.exit_button.enabled = False
+    window.fps_counter.enabled = False
 
-    layout = Layout(side="LEFT")
+    layout_top = Layout(side="TOP")
+
+    layout_left = Layout(side="LEFT")
+
+    layout_bottom = Layout(side="BOTTOM")
+    
+    layout_right = Layout(side="RIGHT")
 
     app.run()
