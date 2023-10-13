b17267c7a34e674d91f103211a7489edaea2ef33
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2023-10-13 19:52:23 +0200

    Update ClickPanel.py

--- ursina.more/ui/ClickPanel.py
+++ ursina.more/ui/ClickPanel.py
@@ -6,11 +6,23 @@ class ClickPanel(Button):
     def __init__(self, scale=(0.3, 0.5), radius=0.01, color=color.black66, visible=False, position=(999, 999),
                  key_control=True,
                  key_bind="right mouse",
+                 button_text="Button1",
+                 button2_text="Button2",
+                 button3_text="Button3",
+                 button4_text="Button4",
+                 button5_text="Button5",
+                 button6_text="Button6",
                  **kwargs):
         super().__init__(paren=camera.ui, model=Quad(radius=radius), scale=scale, color=color, visible=visible,
                          key_control=key_control,
                          key_bind=key_bind,
-                         position=position, z=0)
+                         position=position, z=0,
+                         button_text=button_text,
+                         button2_text=button2_text,
+                         button3_text=button3_text,
+                         button4_text=button4_text,
+                         button5_text=button5_text,
+                         button6_text=button6_text)
 
         if 'color' in kwargs:
             setattr(self, 'color', kwargs['color'])
@@ -24,18 +36,28 @@ class ClickPanel(Button):
         # for i in range(5):
         # c += .14
         # duplicate(self.button, origin=(0, -11.5), text=f"Button{i}", y=-c, z=-1)
-        self.button = Button(parent=self, text="Button1", radius=radius, scale=scale_btn, y=0.4, origin=(0, 0), z=-1,
+        self.button = Button(parent=self, text=self.button_text, radius=radius, scale=scale_btn, y=0.4, origin=(0, 0),
+                             z=-1,
                              highlight_color=clr.azure)
-        self.button2 = Button(parent=self, text="Button2", radius=radius, scale=scale_btn, y=0.26, z=-1,
+        self.button2 = Button(parent=self, text=self.button2_text, radius=radius, scale=scale_btn, y=0.26, z=-1,
                               highlight_color=clr.azure)
-        self.button3 = Button(parent=self, text="Button3", radius=radius, scale=scale_btn, y=0.12, z=-1,
+        self.button3 = Button(parent=self, text=self.button3_text, radius=radius, scale=scale_btn, y=0.12, z=-1,
                               highlight_color=clr.azure)
-        self.button4 = Button(parent=self, text="Button4", radius=radius, scale=scale_btn, y=-0.02, z=-1,
+        self.button4 = Button(parent=self, text=self.button4_text, radius=radius, scale=scale_btn, y=-0.02, z=-1,
                               highlight_color=clr.azure)
-        self.button5 = Button(parent=self, text="Button5", radius=radius, scale=scale_btn, y=-0.16, z=-1,
+        self.button5 = Button(parent=self, text=self.button5_text, radius=radius, scale=scale_btn, y=-0.16, z=-1,
                               highlight_color=clr.azure)
-        self.button6 = Button(parent=self, text="Button6", radius=radius, scale=scale_btn, y=-0.3, z=-1,
+        self.button6 = Button(parent=self, text=self.button6_text, radius=radius, scale=scale_btn, y=-0.3, z=-1,
                               highlight_color=clr.azure)
+
+    def update(self):
+        if self.button6.text == "Exit" or self.button6.text == "exit":
+            self.button6.color = color.rgb(75, 0, 0)
+            self.button6.highlight_color = color.rgb(50, 0, 0)
+            self.button6.on_click = application.quit
+            print("True")
+        else:
+            print("False")
 
     def input(self, key):
         if self.key_control is True:
