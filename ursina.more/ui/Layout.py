3ba9ec9225bee405fb339cfb357116e32d690bec
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2023-11-21 18:58:43 +0100

    Update Layout.py

--- ursina.more/ui/Layout.py
+++ ursina.more/ui/Layout.py
@@ -2,29 +2,34 @@ from ursina import *
 
 
 class Layout(Entity):
-    def __init__(self, **kwargs):
-        super().__init__(model=Quad(radius=.0), color=color.gray, **kwargs)
+    def __init__(self, color=color.gray, z=999, **kwargs):
+        super().__init__(model=Quad(radius=.0), color=color, z=z, **kwargs)
+        self.parent = camera.ui
         self.color = self.color.tint(-.05)
         self.postion = (0, 5)
         self.scale = (10, 5)
+        self.wdw = window.size.x
+        self.wdh = window.size.y
+
+        if 'color' in kwargs:
+            setattr(self, 'color', kwargs['color'])
 
         if "side" in kwargs:
             print(kwargs['side'])
             if kwargs["side"] == "TOP":
-                self.position = (0, 5)
-                self.scale = (15, 5)
+                self.position = window.top
+                self.scale = (self.wdw, 0.25)
             elif kwargs["side"] == "LEFT":
-                self.position = (-8, 0)
-                self.scale = (5, 10)
+                self.position = window.left
+                self.scale = (0.25, self.wdh)
             elif kwargs["side"] == "BOTTOM":
-                self.position = (0, -5)
-                self.scale = (15, 5)
+                self.position = window.bottom
+                self.scale = (self.wdw, 0.25)
             elif kwargs["side"] == "RIGHT":
-                self.position = (8, 0)
-                self.scale = (5, 10)
+                self.position = window.right
+                self.scale = (0.25, self.wdh)
             else:
-                self.position = (0, 5)
-                self.scale = (10, 5)
+                print("Layout can't pack")
 
 
 if __name__ == "__main__":
@@ -34,12 +39,14 @@ if __name__ == "__main__":
     window.exit_button.enabled = False
     window.fps_counter.enabled = False
 
-    layout_top = Layout(side="TOP")
+    layout_top = Layout(side="TOP", color=color.black50)
 
-    layout_left = Layout(side="LEFT")
+    layout_left = Layout(side="LEFT", color=color.red)
 
-    layout_bottom = Layout(side="BOTTOM")
-    
-    layout_right = Layout(side="RIGHT")
+    layout_bottom = Layout(side="BOTTOM", color=color.blue)
+
+    layout_right = Layout(side="RIGHT", color=color.green)
+
+    print(window.size)
 
     app.run()
