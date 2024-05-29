39f76efafec19de65266aa0ab00149f147ae9471
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2024-05-29 21:38:27 +0200

    Update CheckBox.py

--- ursina.more/ui/CheckBox.py
+++ ursina.more/ui/CheckBox.py
@@ -1,27 +1,17 @@
-from ursina import Button, Text, Quad, color
+from ursina import *
 
 
 class CheckBox(Button):
     def __init__(self, start_state=False, **kwargs):
         super().__init__(start_state=start_state, state=start_state, scale=Text.size, model=Quad(radius=.25))
-        self.default_model = None
+        self._state = self.state
+
+        self.state = start_state
 
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-        if 'color' in kwargs:
-            setattr(self, 'color', kwargs['color'])
-        self.highlight_color = self.color.tint(.2)
-        self.pressed_color = self.color.tint(-.2)
-        self.highlight_scale = 1  # multiplier
-        self.pressed_scale = 1  # multiplier
-        self.collider = 'box'
-
-        if CheckBox.default_model is None:
-            if not 'model' in kwargs:
-                self.model = Quad(radius=.25)
-        else:
-            self.model = CheckBox.default_model
+        # self.icon_entity.scale = self.scale
 
     def on_click(self):
         self.state = not self.state
@@ -34,12 +24,17 @@ class CheckBox(Button):
     def state(self, value):
         self._state = value
         print(value)
-        self.text = f' X'[int(value)]
+        if value:
+            self.icon = f"../icons/checkbox.png"
+            self.icon.visible = True
+        else:
+            self.icon.visible = False
 
 
 if __name__ == '__main__':
-    from ursina import Ursina, Slider
+    from ursina import *
+
     app = Ursina()
-    test = CheckBox(model='sphere', start_value=True, color=color.red, scale=.2)
-    Slider(y=-.1)
+    CheckBox(scale=.1)
+    # Slider(y=-.1)
     app.run()
